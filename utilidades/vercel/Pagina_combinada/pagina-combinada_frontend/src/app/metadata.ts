ceda6c5a667ade4f52a0da0eaa788e5651fe12f3
lautarogithub <lautarojuarez2002@gmail.com>
2025-05-11 16:31:09 -0300

    p combinada front next.js y typescript back django

---
+++ utilidades/vercel/Pagina_combinada/pagina-combinada_frontend/src/app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "FerreDesk - Sistema de Gestión",
+  description: "Sistema moderno para la gestión de ferreterías",
+}; 
